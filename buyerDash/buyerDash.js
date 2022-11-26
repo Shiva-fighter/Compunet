eacebee69d07e9c2a87165894023b5c7959a9cc9
SHIVA KASHYAP <93374924+Shiva-fighter@users.noreply.github.com>
2022-11-26 20:04:11 +0530

    create files

---
+++ buyerDash/buyerDash.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AppHeader from "component/AppHeader/AppHeader";
+import AgentDashboard from "modules/private/AgentDashboard/AgentDashboard";
+
+const buyerDash = () => {
+  return (
+    <>
+      <AppHeader />
+      <AgentDashboard />
+    </>
+  );
+};
+export default buyerDash;
